25f80661cd075a4a3cc395d9f3b0179ee051cc4e
andrzone <the.panda.mee@gmail.com>
2023-05-14 18:31:27 +0300

    Повысили скорость передвижения корабля, установили частоту обновления экрана = 60 fps, ограничили перемещения в рамках экрана, добавили кнопку выхода и полноэкранный режим

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -5,6 +5,7 @@ class Ship():
 	def __init__(self, ai_game):
 		# инициализирует корбаль и задает его начальную позицию
 		self.screen = ai_game.screen
+		self.settings = ai_game.settings
 		self.screen_rect = ai_game.screen.get_rect()
 
 		# загружает изображение корабля и получает прямоугольник
@@ -18,12 +19,18 @@ class Ship():
 		self.moving_right = False
 		self.moving_left = False
 
+		# сохранение вещественной координаты корабля
+		self.x = float(self.rect.x)
+
 	def update(self):
+		# обновляется атрибут x, не rect
 		# если moving_right == True
-		if self.moving_right:
-			self.rect.x +=1
-		if self.moving_left:
-			self.rect.x -=1
+		if self.moving_right and self.rect.right < self.screen_rect.right:
+			self.x += self.settings.ship_speed
+		if self.moving_left and self.rect.left > self.screen_rect.left:
+			self.x -= self.settings.ship_speed
+		# обновляем атрибут на основании x
+		self.rect.x = self.x
 
 	def blitme(self):
 		# рисует корабль в текущей позиции
